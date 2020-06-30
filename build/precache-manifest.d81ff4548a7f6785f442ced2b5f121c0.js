b6d180380988ece7a39cbaed253049b6b78ce90f
ProgrammerAvinash <62152948+ProgrammerAvinash@users.noreply.github.com>
2020-06-30 13:53:06 +0530

    build deployed

---
+++ build/precache-manifest.d81ff4548a7f6785f442ced2b5f121c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39efd941053d9912c08a8e758cdabbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f673a0d9bb04d0c4b4a1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c0b54644e69a83cb4422",
+    "url": "/static/js/2.e8203976.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e8203976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f673a0d9bb04d0c4b4a1",
+    "url": "/static/js/main.3c6dcf42.chunk.js"
+  },
+  {
+    "revision": "e517c7748ded850f7311",
+    "url": "/static/js/runtime-main.244bd808.js"
+  }
+]);
